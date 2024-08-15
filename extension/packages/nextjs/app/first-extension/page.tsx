b195d63004995a3c6d08000d89c7123521ac6532
shitover <nftrade.eth@gmail.com>
2024-08-15 18:04:41 +0300

    test

---
+++ extension/packages/nextjs/app/first-extension/page.tsx
@@ -0,0 +1,8 @@
+export default function FirstExtensionPage() {
+    return (
+      <div>
+        <h1>My First Extension</h1>
+        <p>This is a test page for my first extension</p>
+      </div>
+    );
+  }
